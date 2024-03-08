29d74c80fe677cbe4292a41aeac08080b6c25ebf
Emir Nar <emirnar2701@gmail.com>
2024-03-08 13:36:54 +0300

    EmployeeRepo abd EmployeeService has been created

---
+++ src/main/java/com/employeecontrol/demo/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.employeecontrol.demo.service;
+
+import com.employeecontrol.demo.model.Employee;
+import com.employeecontrol.demo.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+}
