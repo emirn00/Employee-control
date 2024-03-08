265ba79c09437e649f890f174c6194f9ab946b7f
Emir Nar <emirnar2701@gmail.com>
2024-03-08 12:14:52 +0300

    employee serviceand controller updated . delete fixed.

--- src/main/java/com/employeecontrol/demo/model/Employee.java
+++ src/main/java/com/employeecontrol/demo/model/Employee.java
@@ -14,7 +14,7 @@ public class Employee implements Serializable {
     private String jobTitle;
     private String phone;
     private String imageUrl;
-    @Column(nullable = false)
+    @Column(nullable = false,updatable = false)
     private String employeeCode;
 
     public Employee(){}
