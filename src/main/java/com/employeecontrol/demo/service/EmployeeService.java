620add23337e2b3ee13bc98fb179245841647520
Emir Nar <emirnar2701@gmail.com>
2024-03-08 13:55:43 +0300

    exception created . Necessary methods implemented.

--- src/main/java/com/employeecontrol/demo/service/EmployeeService.java
+++ src/main/java/com/employeecontrol/demo/service/EmployeeService.java
@@ -1,23 +1,42 @@
 package com.employeecontrol.demo.service;
 
+import com.employeecontrol.demo.exception.UserNotFoundException;
 import com.employeecontrol.demo.model.Employee;
 import com.employeecontrol.demo.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
+    private EmployeeRepo employeeRepo1;
+
     @Autowired
-    public EmployeeService(EmployeeRepo employeeRepo){
+    public EmployeeService(EmployeeRepo employeeRepo) {
         this.employeeRepo = employeeRepo;
     }
 
-    public Employee addEmployee(Employee employee){
+    public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee() {
+        return employeeRepo.findAll();
+    }
+    public Employee findEmployeeById(long id){
+        return  employeeRepo.findEmployeeById(id).orElseThrow(()->new UserNotFoundException("There is no user with" +id +"!!"));
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo1.deleteEmployeeById(id);
     }
 
 }
