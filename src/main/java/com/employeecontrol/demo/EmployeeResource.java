5a484ce290f7d3141a41d1c1cea8631f5082f324
Emir Nar <emirnar2701@gmail.com>
2024-03-08 15:39:15 +0300

    Controller and necessary methods created .

---
+++ src/main/java/com/employeecontrol/demo/EmployeeResource.java
@@ -0,0 +1,48 @@
+package com.employeecontrol.demo;
+
+import com.employeecontrol.demo.model.Employee;
+import com.employeecontrol.demo.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+       List<Employee> employees = employeeService.findAllEmployee();
+       return new ResponseEntity<>(employees , HttpStatus.OK);
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee , HttpStatus.OK);
+    }
+    @PostMapping("/addEmployee")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
